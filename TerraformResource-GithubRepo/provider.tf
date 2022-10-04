4fd47ce9ea8fc7e4226b37c5980ed89f95b556e8
1DevOps2 <anaskhandevops@gmail.com>
2022-10-04 08:39:45 +0000

    TerraformResource-GithubRepo

--- TerraformResource-GithubRepo/provider.tf
+++
@@ -1,9 +0,0 @@
-provider "github" {
-<<<<<<< HEAD
-    token = "<github-token-here>" 
-=======
-    token = "<github-token>"
->>>>>>> c30f174b4258e6c8015f17e5ac211681c8c53c7d
-  
-}
-
---
+++ TerraformResource-GithubRepo/providers.tf
@@ -0,0 +1,5 @@
+provider "github" {
+token = "c30f174b4258e6c8015f17e5ac211681c8c53c7d"
+  
+}
+
